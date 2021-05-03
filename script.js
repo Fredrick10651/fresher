fb7877f68ce2d889d95a576feafff7baf7679903
Fredrick Angose <fredrick.mutsotso@student.moringaschool.com>
2021-05-03 21:05:17 +0300

    It will add updates and comments in java script codes

--- script.js
+++ script.js
@@ -1,87 +1,91 @@
-// Random data: No of cows && Average prod of milk/ltr
-const CowData = {
-    shadeA: {
-      NoOfCows: 30,
-      AvgProduction: 15,
-    },
-    shadeB: {
-      NoOfCows: 19,
-      AvgProduction: 14,
-    },
-    shadeC: {
-      NoOfCows: 21,
-      AvgProduction: 10,
-    },
-    shadeD: {
-      NoOfCows: 39,
-      AvgProduction: 23,
-    } 
-  // will display the CowData per shade
-  };
-  CowData.displayObject("cowData", CowData);
-  console.log(CowData);
-  
-  var totalProdPerShade= function(c,b){
-    return c*b;
-  };
-   let shadeA= totalProdPerShade(30,15);
-   let shadeB= totalProdPerShade(19,14);
-   let shadeC= totalProdPerShade(21,10);
-   let shadeD= totalProdPerShade(39,23);
-  // works out the total calculations of milk produced per shed
-   
-  console.log('Your production in Shed A' ,shadeA, 'litres per day');
-  console.log('Your production in Shed B' ,shadeB, 'litres per day');
-  console.log('Your production in Shed C' ,shadeC, 'litres per day');
-  console.log('Your production in Shed D' ,shadeD, 'litres per day');
-   
-   
-   let totalProduction = shadeA + shadeB + shadeC + shadeD;
-  // works out the total milk produced in all the sheds per day
-   
-   
-   console.log('The total production is',  totalProduction ,'litres per day');
-   let selling_price = 'rate1';
-   let rate1 = 45;
-   let newRate = 49.60;
-   
-   
-   let newIncome = totalProduction * newRate;
-   let newIncomePerDay = totalProduction * rate1;
-   
-   console.log(newIncome, newIncomePerDay);
-   
-   let incomeOverTime = function(time){
-     return newIncomePerDay *time; 
-     
-   };
-   let incomeWeek  = incomeOverTime(7);
-   let incomeYearly  = incomeOverTime(366);
-   
-  
-   console.log('Your weekly income will be Ksh', incomeWeek);
-   console.log('Your yearly income will be Ksh ',incomeYearly);
-   let shades = [shadeA,shadeB,shadeC,shadeD];
-   let shadeName = ['shadeA','shadeB','shadeC','shadeD'];
-   
-  const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  function getNumberOfDaysInMonth(month,year=2021){
-    return new Date(year,month,0).getDate();
+class Dairy {
+  constructor(params) {
+      this._data = {
+          shedA: {
+              cows: 25,
+              productionPerCow: 18
+          },
+          shedB: {
+              cows: 17,
+              productionPerCow: 12
+          },
+          shedC: {
+              cows: 29,
+              productionPerCow: 22
+          },
+          shedD: {
+              cows: 39,
+              productionPerCow: 20
+          },
+      };
+      this._milkPrice = 45;
+      this._monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   }
-  function incomeReport(selling_price=rate1){
-    let i =``;
-    monthName.map(
-      function(element,index){
-        i+=`Your income for ${element} is ${rate1*totalProduction*getNumberOfDaysInMonth(index+1)}
-        `;
-        // should work out the yearly income considering the first rate before increase of the selling_price
-        i+=`Your income for ${element} is ${newRate*totalProduction*getNumberOfDaysInMonth(index+1)}
-        `;
-        // Compares the increase of the new price with the old selling_price
+  // Get the number of milk produced per shed
+  milkProducedPerShed(sp = this._milkPrice) {
+      let jsonReport = {};
+      for (let [key, values] of Object.entries(this._data)) {
+          const total = values.cows * values.productionPerCow * sp;
+          jsonReport[key] = total;
       }
-      );
-      return i;
+      return JSON.parse(JSON.stringify(jsonReport));
   }
-  ;console.log(incomeReport())
-  
-  
+
+  comprehensiveReport(sp = this._milkPrice) {
+      let _data = '';
+      for (let [key, value] of Object.entries(this.milkProducedPerShed(sp))) {
+          _data += `Your production in ${key.charAt(0).toUpperCase() + key.slice(1, 4)} ${key.slice(4)}: ${value} litres per day
+`;
+      }
+      _data += `The total production is ${this.totalProduction()} litres per day`;
+      return _data;
+  }
+  //Total amount earned in a day
+  totalProduction(sp = this._milkPrice) {
+      let x = 0;
+      for (let [key, values] of Object.entries(this.milkProducedPerShed(sp))) {
+          x += values;
+      }
+      return x;
+  }
+  // Get the number of days in a month
+  getDaysInMonth(month, year = 2020) { return (new Date(year, month, 0).getDate()); }
+  //Weekly and yearly income
+  incomeOverTime() {
+      return `Your weekly income will be Ksh ${this.totalProduction() * 7}
+Your yearly income will be Ksh ${this.totalProduction() * 366}`;
+  }
+  // monthly report
+  comprehensiveIncomeOverTimeReport() {
+      let report = '';
+      this._monthNames.map((element, index) => {
+          report += `Your income for ${element} is ${this.totalProduction() * this.getDaysInMonth(index + 1)}
+`;
+      });
+      return report;
+  }
+  // compare the price with previous one when changed
+  compareProfitOnPriceChange(amount) {
+      let report = {};
+      this._monthNames.map((element, index) => {
+          const days = this.getDaysInMonth(index + 1);
+          const previousRate = this.totalProduction() * days;
+          const newRate = this.totalProduction(amount) * this.getDaysInMonth(index + 1);
+          const difference = newRate - previousRate;
+          const profitOrLoss = difference > 0 ? 'Profit' : 'Loss';
+          report[element] = {
+              previousRate,
+              newRate,
+              profitOrLoss
+          };
+      });
+      return report;
+  }
+}
+const bs = new Dairy();
+console.log(bs.compareProfitOnPriceChange(49.6));
+console.log(bs.comprehensiveIncomeOverTimeReport());
+console.log(bs.incomeOverTime());
+console.log(bs.totalProduction());
+console.log(bs.comprehensiveReport());
+console.log(bs.milkProducedPerShed());  
